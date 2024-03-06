83928d0e7e592e2a36d8a8f7317025f17d4e1693
hakkicansengonul <hakkicansengonul@gmail.com>
2024-03-06 16:09:27 +0300

    Setup

---
+++ TurkeyNews/TurkeyNews/Controllers/Core/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TurkeyNews
+//
+//  Created by Hakkı Can Şengönül on 6.03.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
